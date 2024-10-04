ea6ad71e148ab7ab7cb278c4c25f25e0657fe429
P Nikita <92731180+nikita8763@users.noreply.github.com>
2024-10-04 14:04:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import gradio as gr
+
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+def myFunc(message,history):	
+    output = query({
+        "parameters": {"max_new_tokens": 500},
+        "inputs": f"give only sufficient answer nothing extra{message}",
+    })
+
+    return (output[0]['generated_text'])
+
+demo = gr.ChatInterface(fn=myFunc, examples=["code linear search", "merge sort in python", "quote of the day"], title="Chatbot")
+demo.launch()
